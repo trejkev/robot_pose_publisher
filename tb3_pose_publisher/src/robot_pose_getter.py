480720d3ec00c4f09afd82d6e7a969db26c535ae
CERLabRob <trejkev@gmail.com>
2022-10-08 17:27:57 -0600

    Modified to get variables from params

--- tb3_pose_publisher/src/robot_pose_getter.py
+++ tb3_pose_publisher/src/robot_pose_getter.py
@@ -3,43 +3,54 @@
 __author__      = "Kevin Trejos Vargas"
 __email__       = "kevin.trejosvargas@ucr.ac.cr"
 
-"""
-    Description:
-        Robot ground truth pose publisher when simulating through Gazebo. It takes
-        the robotModelName and publish its pose through /robotPosePublisher topic,
-        referenced to referenceModelName, at a rate defined by samplingRate.
-"""
+import rospy                                                                    # Needed for ros and python interaction
+from std_msgs.msg      import Header                                            # Needed to publish the bot message header
+from geometry_msgs.msg import PoseStamped                                       # Needed to publish the bot pose data
+from gazebo_msgs.srv   import GetModelState                                     # Needed to use gazebo service to get the model pose
 
-import rospy                                                                                  # Needed for ros and python interaction
-from std_msgs.msg import Header                                                               # Needed for message header
-from geometry_msgs.msg import PoseStamped                                                     # Needed to publish the bot pose in an evo-allowed format
-from gazebo_msgs.srv import GetModelState                                                     # Needed to use the gazebo service to get the model pose
+rospy.init_node('robotGTPosePublisher')  
 
-#Global variables
-samplingRate       = 20                                                                       # Samples per second to publish the robot pose
-robotModelName     = 'turtlebot3_burger'                                                      # Robot model name
-referenceModelName = 'ground_plane'                                                           # Reference model name
+# -- Tailored message to publish the robot pose
+message = PoseStamped()
+header  = Header()
 
-# Tailored message to publish the robot pose
-message         = PoseStamped()
-header          = Header()
-header.frame_id = '/turtlebot3_burger'
+################################################################################
+############################ -- ASSIGNING PARAMS -- ############################
+################################################################################
 
-rospy.init_node('robotPosePublisher')                                                         # Robot pose publisher node creation
-robotPosePublisher = rospy.Publisher('/robotPosePublisher', PoseStamped, queue_size = 10)     # Robot pose publisher topic name, message type, and queue size statements
-rospy.wait_for_service('/gazebo/get_model_state')                                             # Waiting for gazebo service availability
-actualState = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)                    # Opening gazebo service for getting the robot state
+# -- Getting sampling rate from params
+if rospy.has_param("~sampling_rate"):
+    samplingRate = rospy.get_param("~sampling_rate")
+else:
+    samplingRate = 20
+# -- Getting robot name from params
+if rospy.has_param("~robot_name"):
+    header.frame_id = robotModelName = rospy.get_param("~robot_name")
+else:
+    header.frame_id = robotModelName = 'turtlebot3_burger'  
+# -- Getting publisher topic name from params
+if rospy.has_param("~publisher_topic_name"):
+    publisher_topic_name = rospy.get_param("~publisher_topic_name")
+else:
+    publisher_topic_name = "robotGTPosePublisher"
+# -- Getting reference plane from params
+if rospy.has_param("~reference_plane"):
+    referenceModelName = rospy.get_param("~reference_plane")
+else:
+    referenceModelName = 'ground_plane'
 
-rate = rospy.Rate(samplingRate)                                                               # Samples per second to retrieve the robot pose
+################################################################################
+############################# -- LAUNCHING TOPIC -- ############################
+################################################################################
 
+Publisher = rospy.Publisher(publisher_topic_name, PoseStamped, queue_size = 10)
+rospy.wait_for_service('/gazebo/get_model_state')                               # Waiting for gazebo service availability
+actualState = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)      # Opening gazebo service for getting the robot state
+rate = rospy.Rate(samplingRate)                                                 # Samples per second to get the robot pose
 while not rospy.is_shutdown():
-    result = actualState(robotModelName, referenceModelName)                                  # Get the robotModelName state referenced to referenceModelName
-
-    # Saving into message the robot pose to publish
+    result = actualState(robotModelName, referenceModelName)                    # Get the robotModelName state referenced to referenceModelName
     message.pose   = result.pose
     header.stamp   = rospy.Time.now()
     message.header = header
-
-    robotPosePublisher.publish (message)                                                      # Publish on the topic the robot pose
-
+    Publisher.publish(message)                                                  # Publish on the topic the robot pose
     rate.sleep()
